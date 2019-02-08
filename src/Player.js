004c38adc75a04aee2cd15505b85214930c31e79
rendrap <rendrapradana@gmail.com>
2019-02-08 19:57:28 +0700

    refactor to use Consumer

--- src/Player.js
+++ src/Player.js
@@ -7,42 +7,31 @@ import Icon from './Icon';
 class Player extends PureComponent {
 
   static propTypes = {
-    name: PropTypes.string.isRequired,
-    score: PropTypes.number.isRequired,
-    id: PropTypes.number,
     index: PropTypes.number
   }
 
   render() {
 
     const {
-      name,
-      id,
-      score,
       index,
       isHighScore
     } = this.props;
 
-    console.log(name + ' rendered');
-
     return (
       <div className='player'>
         <Consumer>
-          { context => (
+          { ({ actions, players }) => (
 
             <span className='player-name'>
-            <button className="remove-player" onClick={ () => context.actions.removePlayer(id) }>✖</button>
+            <button className="remove-player" onClick={ () => actions.removePlayer(players[index].id) }>✖</button>
             <Icon isHighScore={isHighScore} />
-              { name }
+              { players[index].name }
             </span>
 
           )}
         </Consumer>
 
-        <Counter
-          score={score}
-          index={index}
-        />
+        <Counter index={index} />
       </div>
     );
   }
--- src/PlayerList.js
+++ src/PlayerList.js
@@ -6,14 +6,13 @@ const PlayerList = () => {
 
   return (
     <Consumer>
-      { context => (
+      { ({ players, highScore }) => (
         <React.Fragment>
-          {context.players.map( (player, index) =>
+          {players.map( (player, index) =>
             <Player
-              {...player}
               key={player.id.toString()}
               index={index}
-              isHighScore={context.highScore === player.score}
+              isHighScore={highScore === player.score}
             />
           )}
         </React.Fragment>
